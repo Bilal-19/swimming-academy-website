442313ad05360f3d3318bf7e1326f5f015b63726
Bilal-19 <bilalmuhammadyousuf543@gmail.com>
2025-01-22 17:23:39 +0500

    Created FAQ page.

---
+++ src/Data/FAQ.jsx
@@ -0,0 +1,37 @@
+export const FAQdata = [
+    {
+        id: 1,
+        question: "What age groups do you cater to for swimming lessons?",
+        answer: "We offer swimming lessons for all age groups, starting from toddlers (3 years and above) to adults. Specialized classes are available for beginners, intermediate, and advanced swimmers."
+    },
+    {
+        id: 2,
+        question: "What are the timings for swimming classes?",
+        answer: "Our swimming classes are conducted in flexible time slots, including mornings, afternoons, and evenings, to accommodate different schedules. You can check the full schedule on our programs page."
+    },
+    
+    {
+        id: 3,
+        question: "Do you provide certified instructors for swimming lessons?",
+        answer: "Yes, all our instructors are certified and experienced in teaching swimming. They are also trained in first aid and water safety to ensure a secure learning environment."
+    },
+
+    {
+        id: 4,
+        question: "Are there any trial classes available before enrollment?",
+        answer: "Yes, we offer one free trial class for new students. This allows you to experience our facilities and teaching methods before committing to a full course."
+    },
+
+    {
+        id: 5,
+        question: "What safety measures are in place at the swimming academy?",
+        answer: "Safety is our top priority. We maintain clean and hygienic pools, have lifeguards on duty during all classes, and provide necessary safety equipment such as floatation aids for beginners."
+    },
+
+    {
+        id: 6,
+        question: "How can I enroll for swimming lessons?",
+        answer: "You can enroll by visit our academy in person. For any assistance, feel free to contact us via email or phone."
+    },
+
+]
--- src/Pages/FAQs.jsx
+++ src/Pages/FAQs.jsx
@@ -1,11 +1,41 @@
 import React from "react";
 import Header from "../Components/Header"
 import Footer from "../Components/Footer"
+import { FAQdata } from "../Data/FAQ";
 
 export default function FAQ() {
     return (
         <>
             <Header />
+
+            <div className="container-fluid mt-5 mb-5">
+                <div className="row">
+                    <div className="col-md-10 mx-auto">
+                        <div className="accordion" id="accordionExample">
+                            {
+                                FAQdata.map(item => {
+                                    return (
+                                        <>
+                                            <div className="accordion-item mb-3">
+                                                <h2 className="accordion-header">
+                                                    <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target={`#collapse${item.id}`} aria-expanded="true" aria-controls={`collapse${item.id}`}>
+                                                        {item.question}
+                                                    </button>
+                                                </h2>
+                                                <div id={`collapse${item.id}`} className="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                                                    <div className="accordion-body">{item.answer}</div>
+                                                </div>
+                                            </div>
+                                        </>
+                                    )
+                                })
+                            }
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
             <Footer />
         </>
     )
