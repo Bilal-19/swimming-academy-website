1a4f6bebc24663f23074eced1fabf8a1e0b65108
Bilal-19 <bilalmuhammadyousuf543@gmail.com>
2025-01-22 15:07:47 +0500

    Fixed bugs.

--- src/Pages/Gallery.jsx
+++ src/Pages/Gallery.jsx
@@ -12,24 +12,18 @@ export default function Gallery() {
             <div className="container-fluid">
                 <div className="row m-4">
                     <div className="col-md-11 mx-auto">
-                        <div id="carouselExample" className="carousel slide">
-                            <div class="carousel-inner">
-                                <div className="carousel-inner">
-                                    {GalleryImages.map((image, index) => (
-                                        <div key={image.id} className={`carousel-item ${index === 0 ? "active" : ""}`}>
-                                            <img
-                                                src={image.imgURL}
-                                                className="d-block w-100 carousel-img"
-                                                alt={image.imgCaption}
-                                            />
-                                            <div className="carousel-caption d-none d-md-block">
-                                                <h5>{image.imgCaption}</h5>
-                                            </div>
+                        <div id="carouselExample" class="carousel slide">
+                            <div className="carousel-inner">
+                                {GalleryImages.map((value, index) => (
+                                    <div key={value.id} className={`carousel-item ${index === 0 ? "active" : ""}`}>
+                                        <img src={value.imgURL} className="d-block w-100 carousel-img" alt={value.imgCaption} />
+                                        <div className="carousel-caption d-none d-md-block">
+                                            <h5>{value.imgCaption}</h5>
                                         </div>
-                                    ))}
-                                </div>
-
+                                    </div>
+                                ))}
                             </div>
+
                             <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
                                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                                 <span class="visually-hidden">Previous</span>
